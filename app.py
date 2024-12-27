6689f764343d3322be7540f179e1c37f66ccf96a
Ana <bayleyosornio@gmail.com>
2024-12-27 15:17:45 -0600

    fixed post habits

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask 
+import json
 app = Flask (__name__)
 
 @app.route("/")
@@ -6,3 +7,16 @@ def home():
 	return "Hello, World!"
 if __name__ == "__main__":
 	app.run(debug=True)
+
+habits = {
+	1: {"name":"Drink water", "frequency" : "daily"},
+	2: {"name": "Work out", "frequency": "weekly"}
+}
+
+@app.route ("/habits", methods = ["POST"])
+def create_habit():
+	data = request.json
+	name =data.get("name")
+	frequency = data.get("frequency")
+	if not name or not frequency:
+		return jsonify({"error": "Both 'name' and 'frequency' are required"}), 400
